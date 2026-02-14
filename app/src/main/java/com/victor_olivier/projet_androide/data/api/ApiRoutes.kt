96e8f15d3708dc57439fe0bdf63d9a0fc1a52c95
Victor OLIVIER <131671439+Victor3699@users.noreply.github.com>
2026-02-14 16:16:40 +0100

    Pré-initialise la session web API dès la connexion

--- app/src/main/java/com/victor_olivier/projet_androide/data/api/ApiRoutes.kt
+++ app/src/main/java/com/victor_olivier/projet_androide/data/api/ApiRoutes.kt
@@ -7,6 +7,8 @@ object ApiRoutes {
 
     const val HOUSES = "$BASE/api/houses"
     fun DEVICES(houseId: Int) = "$BASE/api/houses/$houseId/devices"
+    fun DEVICE_COMMAND(houseId: Int, deviceId: String, command: String) =
+        "$BASE/api/houses/$houseId/devices/$deviceId/commands/$command"
 
     fun HOUSE_BROWSER(houseId: Int) = "$BASE?houseId=$houseId"
 }
